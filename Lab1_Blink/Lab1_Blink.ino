2bd6d77dac6119139dabe759f8cb2725971ded88
Miles Cramer <02miles.cramer@gulllakecs.org>
2019-11-14 14:15:25 -0500

    Add initial program for digitalread()

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -7,14 +7,24 @@
  * to the serial monitor
  */
 
+//digital pin2 has a push button attached to it
+int pushButton = 2;
 
 
 
 void setup() {
-
+  //This initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+  //make the pushbutton pin an input
+  pinMode(pushbutton, INPUT)
  
 }
 
 void loop() {
+  // read the input pin
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button 
+  Serial.println(buttonState);
+  delay(250); // delay in between readings for stability
 
 }
